56efa9514e4d75498eff292ee4d7c41bb7ecfc7f
Realwale <olawaleagboola41@gmail.com>
2023-10-13 09:55:46 +0100

    initialize project

---
+++ src/main/java/com/progresoft/fxdealsdatawarehouse/FxDealsDatawarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.progresoft.fxdealsdatawarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxDealsDatawarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxDealsDatawarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/progresoft/fxdealsdatawarehouse/FXDealsDatawarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progresoft.fxdealsdatawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FXDealsDatawarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
